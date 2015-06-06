6bb17b2f0386aa2c07513031dfcb90f898a287e8
Jeroen Heijmans <github@jeroenheijmans.nl>
2015-06-06 14:25:07 +0200

    Implements undo/redo stacks on Matches

--- src/js/match.js
+++ src/js/match.js
@@ -1,5 +1,33 @@
 (function (cf) {
-	cf.Match = function() {
+	cf.Match = function(board) {
+		var self = this,
+			redoStack = [],
+			undoStack = [];
 
+		self.canRedo = function() {
+			return redoStack.length > 0;
+		}
+
+		self.canUndo = function() {
+			return undoStack.length > 0;
+		}
+
+		self.doMove = function(move) {
+			undoStack.push(move);
+			move.redo(board);
+			redoStack.length = 0;
+		}
+
+		self.redo = function() {
+			var move = redoStack.pop();
+			move.redo(board);
+			undoStack.push(move);
+		}
+
+		self.undo = function() {
+			var move = undoStack.pop();
+			move.undo(board);
+			redoStack.push(move);
+		}
 	};
 }(ConnectFour || {}));
--- test/match.tests.js
+++ test/match.tests.js
@@ -1,9 +1,67 @@
 (function (cf, test) {
+	var fakeMove;
+
 	QUnit.module("Matches", {
+		beforeEach: function() {
+			fakeMove = { 
+				redo: function(_) { },
+				undo: function(_) { }
+			}
+		}
 	});
 
 	test("Can create default match", function(assert){
 		var match = new cf.Match();
 		assert.ok(!!match);
-	});	
+	});
+
+	test("Default match will have no redo", function(assert){
+		var match = new cf.Match();
+		assert.strictEqual(match.canRedo(), false);
+	});
+
+	test("Default match will have no undo", function(assert){
+		var match = new cf.Match();
+		assert.strictEqual(match.canUndo(), false);
+	});
+
+	test("Match can undo right after doing a move", function(assert) {
+		var match = new cf.Match();
+		match.doMove(fakeMove);
+		assert.strictEqual(match.canUndo(), true);
+	});
+
+	test("Match can redo right after undo", function(assert) {
+		var match = new cf.Match();
+		match.doMove(fakeMove);
+		match.undo();
+		assert.strictEqual(match.canRedo(), true);
+	});
+
+	test("Undo the first move makes undo impossible", function(assert) {
+		var match = new cf.Match();
+		match.doMove(fakeMove);
+		match.undo();
+		assert.strictEqual(match.canUndo(), false);
+	});
+
+	test("Redo is not possible on fresh match", function(assert) {
+		var match = new cf.Match();
+		assert.strictEqual(match.canRedo(), false);
+	});
+
+	test("Undo is not possible on fresh match", function(assert) {
+		var match = new cf.Match();
+		assert.strictEqual(match.canUndo(), false);
+	});
+
+	test("Doing a fresh move clears redo stack", function(assert) {
+		var match = new cf.Match();
+		match.doMove(fakeMove);
+		match.undo();
+		match.doMove(fakeMove);
+		assert.strictEqual(match.canRedo(), false);
+	});
+
+
 }(ConnectFour, QUnit.test));
