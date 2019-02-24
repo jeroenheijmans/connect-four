759bd2065eb8c636c636a88a4d2f6fa2409f9cee
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-02-24 22:13:22 +0100

    Update webpack for css (module 13)

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
+/* eslint-disable */
+import normalize from '../../node_modules/normalize.css/normalize.css'
+import styles from '../css/styles.css'
+
 import { connectFourApp } from "./app/app";
 
-// eslint-disable-next-line angular/module-getter
 connectFourApp.run(["gameMaster", function(gameMaster) { gameMaster.startNewMatch(); }]);
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,36 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
   mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
-      { test: require.resolve('angular'), loader: 'exports-loader?window.angular' },
+      {
+        test: require.resolve('angular'),
+        loader: 'exports-loader?window.angular'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ]
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    })
   ],
 };
