85aba311feb956d1c7687f6c5cd632049ba0f7a0
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-03-10 14:24:16 +0100

    Add proxy magic (module 17)

--- src/js/domain/slot.js
+++ src/js/domain/slot.js
@@ -1,3 +1,8 @@
+const propertiesWithWatchers = [
+  'clear',
+  'setPlayer',
+];
+
 export default class Slot {
   constructor(rowIndex, colIndex) {
     this.takenByPlayer = null;
@@ -5,6 +10,28 @@ export default class Slot {
 
     this.getRowIndex = () => rowIndex;
     this.getColIndex = () => colIndex;
+
+    return new Proxy(this, {
+      get(target, propertyName, _receiver) {
+        if (propertiesWithWatchers.includes(propertyName)) {
+          const func = target[propertyName];
+
+          // TODO: Memoization?
+          return function(...args) {
+            const before = target.takenByPlayer;
+            const result = func.apply(this, args);
+
+            if (target.takenByPlayer !== before) {
+              target.notifyStateChangeSubscribers.apply(this);
+            }
+
+            return result;
+          }
+        }
+
+        return target[propertyName];
+      },
+    });
   }
 
   notifyStateChangeSubscribers() {
@@ -30,7 +57,7 @@ export default class Slot {
     }
 
     this.takenByPlayer = player;
-    this.notifyStateChangeSubscribers();
+    
   }
 
   clear() {
@@ -39,6 +66,5 @@ export default class Slot {
     }
 
     this.takenByPlayer = null;
-    this.notifyStateChangeSubscribers();
   }
 }
--- test/board.tests.js
+++ test/board.tests.js
@@ -6,8 +6,8 @@ let fakeYellowPlayer, fakeRedPlayer;
 
 QUnit.module("Boards", {
   beforeEach: () => {
-    fakeYellowPlayer = {};
-    fakeRedPlayer = {};
+    fakeYellowPlayer = { yellow: true };
+    fakeRedPlayer = { red: true };
   }
 });
 
